d82efeb86b53241e2efbea97c7586975bdee0e67
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-03-24 01:36:13 +0900

    이미지 크롤링  및 DB 저장 기능추가
    image_crawling.py에서  url에 따른 이미지를 크롤링하고 test.py를 실행하여 db에 저장함(교수님 시연용)

---
+++ Place_crawling/image_crawling.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+from selenium.webdriver.common.by import By
+import time
+
+
+# 일단 음식점 하나만!
+
+
+def image_crawling(driver, link, cnt):
+
+    img_list = []
+    num = 1
+    # cnt = int(input("가져오고 싶은 이미지 개수를 입력하세요 : "))
+    print("-" * 50)
+    name = driver.find_element(
+        by=By.XPATH,
+        value="/html/body/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div[1]/span[1]",
+    )
+    print(name.text)
+
+    while num < cnt + 1:
+
+        try:
+            path = driver.find_element(
+                by=By.XPATH,
+                value=f'//*[@id="app-root"]/div/div/div[2]/div[5]/div/div[1]/ul/li[{num}]',
+            )
+            try:
+                image_path = driver.find_element(
+                    by=By.XPATH, value=f'//*[@id="visitor_{num}"]'
+                )
+            except:
+                image_path = driver.find_element(
+                    by=By.XPATH, value=f'//*[@id="ugc_{num}"]'
+                )
+            image = image_path.get_attribute("src")
+            print("num :", num)
+            print(image)
+            img_list.append(image)
+        except:
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+            driver.implicitly_wait(5)
+        #             time.sleep(5)
+        num += 1
+        return img_list
+
+
+def total_img_list_func(url, cnt):
+    total_image_list = []
+
+    options = webdriver.ChromeOptions()
+    # 창 숨기는 옵션 추가
+    options.add_argument("headless")
+    driver = webdriver.Chrome("chromedriver", options=options)
+
+    driver.get(url[:-4] + "photo")
+
+    driver.implicitly_wait(5)
+
+    total_image_list += image_crawling(driver, url, cnt)
+    return total_image_list
--- Place_crawling/test(to database).py
+++ Place_crawling/test(to database).py
@@ -1,10 +1,10 @@
-
 import certifi
 from models.information import Information
 from pymongo import MongoClient
 from config import MONGO_URL
-from search_restaurant_url import restaurant # 지역별 음식점의 링크 가져오기 
-from inform_restaurant import inform_restaurant #네이버 플레이스에서 음식점에 대한 정보 가져오기 
+from search_restaurant_url import restaurant  # 지역별 음식점의 링크 가져오기
+from inform_restaurant import inform_restaurant  # 네이버 플레이스에서 음식점에 대한 정보 가져오기
+from image_crawling import total_img_list_func  # 이미지 크롤링 후 리스트 가져오기
 
 if __name__ == "__main__":
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     for url in urls:
 
         result = inform_restaurant(url)
-
+        total_img = total_img_list_func(url, 1)  # 각 url마다 몇 개의 이미지를 가져올것인가
         info = {
             "name": result["이름"],
             "sort": result["분류"],
@@ -27,9 +27,9 @@ if __name__ == "__main__":
             "mean_price": result["평균 가격"],
             "point": float(result["평점"]),
             "reviews": int(result["리뷰 수"]),
+            "image": total_img[0],  # html 시험용으로 하나만
         }
 
-        dpInsert = db.inform.insert_one(info) # db에 정보 입력 
+        dpInsert = db.inform.insert_one(info)  # db에 정보 입력
         # df = pd.concat([df, pd.DataFrame(result)], ignore_index=False)
     print("finish!")
- 
