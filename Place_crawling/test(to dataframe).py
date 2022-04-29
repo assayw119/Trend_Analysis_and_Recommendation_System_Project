77737dce789b182b54e4e2a0f709948205b5d84b
YoungWoong Park <assayw119@naver.com>
2022-04-29 18:58:39 +0900

    Revert "0426 네이버 크롤링 함수 코드 수정"
    This reverts commit a7ae6d1ab8a623e4d62ca89127064a587d40c944.

--- Place_crawling/test(to dataframe).py
+++ Place_crawling/test(to dataframe).py
@@ -1,71 +1,29 @@
 import time
 from selenium import webdriver
+import logging
+from selenium.webdriver.remote.remote_connection import LOGGER
+
 import pandas as pd
+from config import MONGO_URL
 from search_restaurant_url import restaurant  # 지역별 음식점의 링크 가져오기
 from inform_restaurant import inform_restaurant  # 네이버 플레이스에서 음식점에 대한 정보 가져오기
-from naver_reviews import naver_reviews_list  # 네이버 플레이스에서 음식점에 대한 리뷰 가져오기
-from image_crawling import image_crawling  # 네이버 플레이스에서 음식점에 대한 이미지 가져오기
+from naver_reviews import naver_reviews_list
+from image_crawling import image_crawling
 import warnings
 
 warnings.filterwarnings("ignore")
+if __name__ == "__main__":
+    region_df = pd.read_csv("/Users/seop/Downloads/Report.csv")
 
+    region_df = region_df.drop(0, axis=0)
+    region_df = region_df.drop(1, axis=0)
+    region_df = region_df.drop(2, axis=0)
 
-def crawling(urls, region):
+    i = 0
+    start = time.time()
+    for region in region_df["법정동"][393:]:
 
-    global df
-    for idx, url in enumerate(urls):
-
-        print(f"{idx}번째 URL")
-        s = time.time()
-        result = inform_restaurant(url)
-        options = webdriver.ChromeOptions()
-
-        # 창 숨기는 옵션 추가
-        options.add_argument("headless")
-
-        driver1 = webdriver.Chrome(
-            "/Users/seop/Documents/GitHub/Trend_Analysis_and_Recommendation_System_Project/Place_crawling/chromedriver",
-            options=options,
-        )
-        driver1.get(url[:-4] + "review/visitor")
-        driver1.implicitly_wait(10)
-
-        driver2 = webdriver.Chrome(
-            "/Users/seop/Documents/GitHub/Trend_Analysis_and_Recommendation_System_Project/Place_crawling/chromedriver",
-            options=options,
-        )
-        driver2.get(url[:-4] + "photo?filterType=음식")
-        driver2.implicitly_wait(10)
-
-        driver3 = webdriver.Chrome(
-            "/Users/seop/Documents/GitHub/Trend_Analysis_and_Recommendation_System_Project/Place_crawling/chromedriver",
-            options=options,
-        )
-        driver3.get(url[:-4] + "photo?filterType=내부")
-        driver3.implicitly_wait(10)
-
-        review_list = naver_reviews_list(driver1, url, 50)
-        result["review_list"] = "/".join(review_list)
-
-        img_food = image_crawling(driver2, url, 5)
-        result["img_food"] = ",".join(img_food)
-
-        img_inner = image_crawling(driver3, url, 5)
-        result["img_inner"] = ",".join(img_inner)
-
-        try:
-            df = pd.concat([df, pd.DataFrame(result, index=[idx])], ignore_index=False)
-        except:
-            pass
-        e = time.time()
-        print("time for each loop : ", e - s, "s")
-
-    df.to_csv(f"{region}.csv", encoding="utf-8-sig")
-
-
-def start_crawling(region_df):
-    for region in region_df["법정동"]:
-
+        s2 = time.time()
         print("현재 지역 :", region)
         urls = restaurant(region, 3)  # 지역, 음식점 갯수
         df = pd.DataFrame(
@@ -79,22 +37,69 @@ def start_crawling(region_df):
                 "people_give_score",
                 "review_count",
                 "review_list",
+                dasd
+                asdas
+                ß
                 "img_food",
                 "img_inner",
             ]
         )
+        error = []
+        dong = []
+        for idx, url in enumerate(urls):
 
-        crawling(urls, region)
+            print(f"{idx}번째 URL")
+            s = time.time()
+            result = inform_restaurant(url)
+            options = webdriver.ChromeOptions()
 
+            # 창 숨기는 옵션 추가
+            options.add_argument("headless")
 
-def main():
+            driver1 = webdriver.Chrome(
+                "/Users/seop/Documents/GitHub/Trend_Analysis_and_Recommendation_System_Project/Place_crawling/chromedriver",
+                options=options,
+            )
+            driver1.get(url[:-4] + "review/visitor")
+            driver1.implicitly_wait(10)
 
-    region_df = pd.read_csv("/Users/seop/Downloads/Report.csv")
-    region_df = region_df.drop(index=[0, 1, 2], axis=0)
-    start_crawling(region_df)
+            driver2 = webdriver.Chrome(
+                "/Users/seop/Documents/GitHub/Trend_Analysis_and_Recommendation_System_Project/Place_crawling/chromedriver",
+                options=options,
+            )
+            driver2.get(url[:-4] + "photo?filterType=음식")
+            driver2.implicitly_wait(10)
 
+            driver3 = webdriver.Chrome(
+                "/Users/seop/Documents/GitHub/Trend_Analysis_and_Recommendation_System_Project/Place_crawling/chromedriver",
+                options=options,
+            )
+            driver3.get(url[:-4] + "photo?filterType=내부")
+            driver3.implicitly_wait(10)
 
-if __name__ == "__main__":
-    start = time.time()
-    main()
+            review_list = naver_reviews_list(driver1, url, 50)
+            result["review_list"] = "/".join(review_list)
+
+            img_food = image_crawling(driver2, url, 5)
+            #     print('img_food :',img_food)
+            result["img_food"] = ",".join(img_food)
+
+            img_inner = image_crawling(driver3, url, 5)
+            result["img_inner"] = ",".join(img_inner)
+
+            try:
+
+                df = pd.concat(
+                    [df, pd.DataFrame(result, index=[idx])], ignore_index=False
+                )
+            except:
+                error.append(url)
+            e = time.time()
+            print("time for each loop : ", e - s, "s")
+
+        e2 = time.time()
+
+        df.to_csv(f"{region}.csv", encoding="utf-8-sig")
+
     end = time.time()
+    print("total running time :", end - start, "sec")
