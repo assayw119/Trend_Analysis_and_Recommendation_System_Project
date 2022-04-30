a9294f05db26f692a1240df45946a4308d57e240
YoungWoong Park <assayw119@naver.com>
2022-04-30 22:42:17 +0900

    쓸모없는 코드 삭제

--- Webpage_2/app.py
+++ Webpage_2/app.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, jsonify
 import pymysql
-import db_connect
 
 app = Flask(__name__)
 
-db = pymysql.connect(host='localhost', user='root', password='qwedsa2249',
-                     db='test', charset='utf8')
-cursor = db.cursor()
-db.commit()
 
 ## HTML을 주는 부분
 @app.route('/')
@@ -20,24 +15,12 @@ def result():
 
 @app.route('/resultdata', methods=['GET'])
 def select():
-   # db_class = db_connect.Database()
-   #
-   # sql = 'SELECT name FROM test'
-   # row = db_class.executeAll(sql)
 
    db = pymysql.connect(host='localhost', user='root', password='qwedsa2249',
                         db='test', charset='utf8')
    cursor = db.cursor()
-   # db.commit()
-   # engine = create_engine('mysql+pymysql://root:qwedsa2249@localhost:3306/test?charset=utf8', encoding='utf-8')
-   # utf8로 이모티콘 등의 문자 인식되지 않는 것 해결 필요
-
-   # conn = engine.connect()
-   # df_name.to_sql(name='chungmuro', con=engine, if_exists='replace', index=False)
-   # conn.close()
 
    sql = 'select * from chungmuro'
-   # row = pd.read_sql(sql, db)
    cursor.execute(sql)
    row = cursor.fetchall()
    # print(row)
