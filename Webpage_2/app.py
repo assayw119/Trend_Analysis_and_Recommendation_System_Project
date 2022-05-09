7bc747a0a4aa92d136bac49052b7124f4d9a820c
YoungWoong Park <assayw119@naver.com>
2022-05-09 23:52:25 +0900

    paging의 용이성을 위해 ajax가 아닌 render를 이용해 python으로 직접 templates에 내용 삽입

--- Webpage_2/app.py
+++ Webpage_2/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import pymysql
+import pandas as pd
+from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
 
+db = pymysql.connect(host='localhost', user='root', password='qwedsa2249',
+                        db='test', charset='utf8')
+cursor = db.cursor(pymysql.cursors.DictCursor)
 
 ## HTML을 주는 부분
 @app.route('/')
@@ -11,20 +16,59 @@ def home():
 
 @app.route('/result')
 def result():
-   return render_template('02_result_page.html')
+   page = request.args.get('page', 1, type=int)
+
+   limit = 10
+   sql = 'select * from chungmuro;'
+   cursor.execute(sql)
+   datas_dict = cursor.fetchall()
+
+   datas = pd.DataFrame(datas_dict)
+
+   total_cnt = len(datas)
+   total_page = round(total_cnt / limit)
+
+   block_size = 5
+   block_num = int((page-1) / block_size)
+   block_start = (block_size * block_num) + 1
+   block_end = block_start + (block_size - 1)
+
+   return render_template('02_result_page.html',
+                          datas = datas,
+                          limit = limit,
+                          page = page,
+                          block_start = block_start,
+                          block_end = block_end,
+                          total_page = total_page,)
+
+@app.route('/detail')
+def detail():
+   return render_template('03_detail_page.html')
+
 
 @app.route('/resultdata', methods=['GET'])
 def select():
 
-   db = pymysql.connect(host='localhost', user='root', password='qwedsa2249',
-                        db='test', charset='utf8')
-   cursor = db.cursor()
 
-   sql = 'select * from chungmuro'
-   cursor.execute(sql)
-   row = cursor.fetchall()
+   # count_sql = 'select count(*) from chungmuro'
+   # cursor.execute(count_sql)
+   # total_cnt = cursor.fetchall()
+   # print(total_cnt[0][0])
+   # total_page = round(total_cnt / per_page)
+
+   sql2 = 'select * from chungmuro;'
+   cursor.execute(sql2)
+   row2 = cursor.fetchall()
+
+
    # print(row)
-   return jsonify({'data':row})
+   return jsonify({'data':row2})
+
+# @app.route('/resultdata', method=['POST'])
+# Default setting
+# pageLimit = 10
+
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5001,debug=True)
--- Webpage_2/static/02_result_page.js
+++ Webpage_2/static/02_result_page.js
@@ -27,3 +27,55 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 })
 
+//
+// $(document).ready(function () {
+//     restaurant();
+// });
+//
+// function restaurant() {
+//     $.ajax({
+//         type: "GET",
+//         url: "/resultdata",
+//         data: {},
+//         success: function (response) {
+//             alert('success!')
+//             let data = response['data']
+//
+//             for (let i=0; i<data.length; i++) {
+//                 let number = i+1
+//                 let name = data[i][0]
+//                 let address = data[i][1]
+//                 // let sort = data[i]['sort']
+//                 // let menu = data[i]['menu']
+//                 // let mean_price = data[i]['mean_price']
+//                 let score = data[i][5]
+//                 // let people_give_score = data[i]['people_give_score']
+//                 // let review_count = data[i]['review_count']
+//                 let review_list = (data[i][8]||'').split('/')
+//                 let img_food = data[i][9]
+//                 if (img_food == null) {
+//                     img_food = '../static/assets/img/img_nothing.jpeg'
+//                 } else {
+//                     img_food = (data[i][9]||'').split(',')[0]
+//                 }
+//
+//                 // let img_inner = data[i]['img_inner']
+//
+//                 let temp_html = `<h2 class="post-title" style="left">${number}. ${name}</h2>
+//                                     <a href="https://map.naver.com/v5/search/${name}/place/" target="_blank">
+//                                     <img src= "${img_food}"
+//                                         class="food-img" style="float:left;">
+//                                     <h3 class="post-score" style="float:right">평점: ${score}</h3>
+//                                     <h3 class="post-address"">${address}</h3>
+//
+//                                     <p class="post-review">
+//                                         ${review_list[0]}
+//                                     </p>
+//                                 </a>
+//                                 <!-- Divider-->
+//                                 <hr class="my-5" />`
+//                 $('#restaurant-box').append(temp_html)
+//             }
+//         }
+//     })
+// }
