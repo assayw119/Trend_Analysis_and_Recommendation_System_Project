711c5f83ba04ee25fef078eb3637a92b050627e7
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-05-16 23:01:01 +0900

    0516
    카카오톡 크롤러 수정
    동별로 5개씩 음식점 명으로 크롤링 할 수 있도록 수정하는 중

--- Place_crawling/kakao_search_restaurant_url.py
+++ Place_crawling/kakao_search_restaurant_url.py
@@ -3,7 +3,8 @@ def kakao_restaurant(station, cnt):  # 15의 배수로 입력(15,30,....,150...)
 
     app_key = "f58ecd8a6f3a91c7b483596610cb46f9"
     headers = {"Authorization": "KakaoAK {}".format(app_key)}
-    query = station + "맛집"
+    # query = station + "맛집"
+    query = station
     pages = range(cnt // 15)  # 1페이지당 15개의 음식점의 정보를 불러올 수 있다.
     url = (
         "https://dapi.kakao.com/v2/local/search/keyword.json?query={}&pages={}".format(
