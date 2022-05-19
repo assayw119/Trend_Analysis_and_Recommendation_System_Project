eb64ad8c65b64b721626e4a15ef511efee790fc6
YoungWoong Park <assayw119@naver.com>
2022-05-19 17:00:06 +0900

    Revert "merge"
    This reverts commit a59f307cd358793bfea199d5d538ff967d7478bd.

---
+++ Place_crawling/image_crawling.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+from selenium.webdriver.common.by import By
+import time
+
+
+# 일단 음식점 하나만!
+
+cnt = 1
+
+
+def image_crawling(driver, link, cnt):
+    scroll_repeat = cnt // 30
+    for i in range(scroll_repeat):
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        driver.implicitly_wait(5)
+    img_list = []
+    num = 1
+    time.sleep(2)
+
+    image_path = driver.find_elements(
+        by=By.CLASS_NAME,
+        value="_img",
+    )
+
+    for image in image_path:
+        img = image.get_attribute("src")
+
+        img_list.append(img)
+
+    return img_list
+
+
+# options = webdriver.ChromeOptions()
+# # 창 숨기는 옵션 추가
+# options.add_argument("headless")
+# driver = webdriver.Chrome("/Users/seop/파이썬/아아아하기싫어/chromedriver_mac64_m1 (4)/chromedriver", options=options)
---
+++ Place_crawling/병렬_crawling.py
@@ -0,0 +1,4 @@
+from search_restaurant_url import restaurant
+
+url = restaurant("충무로", 1)
+print(url)
