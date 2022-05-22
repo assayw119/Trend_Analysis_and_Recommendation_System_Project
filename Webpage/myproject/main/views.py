ee157c0ca53fb12234e07c9b74684943937387a0
YoungWoong Park <assayw119@naver.com>
2022-05-23 03:53:15 +0900

    - 유입페이지 path parameter 받아오기 및 url 넘기기 구현중...

--- Webpage/myproject/main/views.py
+++ Webpage/myproject/main/views.py
@@ -2,6 +2,7 @@ from unicodedata import name
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Demo
 from django import template
+from .forms import RestaurantForm
 
 def showhome(request):
     img = Demo.objects.all() # 이미지만 못가져올까
@@ -10,7 +11,7 @@ def showhome(request):
         cluster_img.append(Demo.objects.filter(clustering=i).first())
     return render(request, 'main/01_inflow_page.html', {'imgs':img, 'cluster_imgs':cluster_img})
 
-def showresult(request):
+def showresultall(request):
     # sql = 'select * from demo'
     # database = Database()
     # data = database.executeAll(sql)
@@ -23,12 +24,19 @@ def showresult(request):
 def showdetail(request):
     return render(request, 'main/03_detail_page.html')
 
-def showcluster(request):
-    # data = []
-    # length = ['0','1','2','3','4']
-    # for i in length:
-    #     val = Demo.objects.filter(clustering=i)
-    #     data.append(val)
-    #     continue
-    data = Demo.objects.all()
-    return render(request, 'main/01_inflow_page.html', {'cluster_imgs' : data})
+def category(request):
+    if request.method=='GET':
+        sido = request.GET.get('sido')
+        sigugun = request.GET.get('sigugun')
+        dong = request.GET.get('gun')
+        img = request.GET.get('inner_img')
+        food = request.GET.get('food')
+
+        restaurant = RestaurantForm()
+        restaurant.sido = sido
+        restaurant.sigugun = sigugun
+        restaurant.dong = dong
+        restaurant.img = img
+        restaurant.food = food
+        restaurant.save()
+        return redirect('main/02_result_page.html', restaurant)
