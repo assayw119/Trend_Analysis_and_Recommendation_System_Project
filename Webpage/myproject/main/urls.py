3384157a43cf12f643475606c27aa587b569bdbd
YoungWoong Park <assayw119@naver.com>
2022-06-05 02:17:52 +0900

    url include 작업 진행

---
+++ Webpage/myproject/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name = 'main'
+urlpatterns = [
+    path('', showhome, name='showhome'),
+    path('resultall/', showresultall, name='showresultall'),
+    path('result/<str:id>', showdetail, name='showdetail'),
+    # path('sort', sortresult, name='sortresult'),
+
+]
